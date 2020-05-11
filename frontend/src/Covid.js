480cac7ad7dd5998ca07f61d6b0eccbbb9b608c9
Hadla <hadla.bergman@gmail.com>
2020-05-11 20:49:23 +0200

    Styled Covid component.

--- frontend/src/Covid.js
+++ frontend/src/Covid.js
@@ -1,89 +1,77 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
 import axios from 'axios';
 import { states } from './usStates';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
 
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+
+
 
 class Covid extends Component {
+
+
     constructor() {
         super();
         this.state = {
-            usState: "Alaska",
-            hospitalizedCurrently: "five",
-            totalDeaths: "two",
             covidData: []
         }
     }
 
     componentDidMount = () => {
         this.getCovidData()
-
     }
 
-    async getCovidData()  {
-        let deathData = await axios.get("/death");
-        let currentData = await axios.get("/current");
-        let finalData = currentData.data.map(currentElement => {
-            let death = deathData.data.find(deathElement => 
-                currentElement.usState === deathElement.usState
-            )
-            return {...currentElement, death: death.totalDeaths}
-        })
-        this.setState({
-            ...this.state,
-            covidData: finalData
-        })
-        // return axios.get("/current")
-        //     .then(body => {
-        //         console.log(body);
-        //         return this.setState({
-        //             ...this.state,
-        //             covidData: body.data
-        //         })
-        //     })
+    async getCovidData() {
+        try {
+            let deathData = await axios.get("/death");
+            let currentData = await axios.get("/current");
+            let finalData = currentData.data.map(currentElement => {
+                let death = deathData.data.find(deathElement =>
+                    currentElement.usState === deathElement.usState
+                )
+                return { ...currentElement, death: death.totalDeaths }
+            })
+            this.setState({
+                ...this.state,
+                covidData: finalData
+            })
+        } catch (error) {
+            console.log(error.message)
+        }
     }
+
     covidDataElements() {
+
         console.log(this.state.covidData);
         return (
-        <TableContainer component={Paper}>
-      <Table className="table" aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell >State</TableCell>
-            <TableCell align="left">Hospitalized</TableCell>
-            <TableCell align="left">Death</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {this.state.covidData.map((row, index) => (
-            <TableRow key={'covidRow'+index}>
-              
-              <TableCell align="left">{states[row.usState]}</TableCell>
-              <TableCell align="left">{row.hospitalized || 'No data'}</TableCell>
-              <TableCell align="left">{row.death || 'No data'}</TableCell>
- 
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>)
-       
+            <TableContainer component={Paper}>
+                <Table className="table" aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell style={{fontWeight: "800"}} >State</TableCell>
+                            <TableCell className="hospitalized" align="left">Hospitalized</TableCell>
+                            <TableCell style={{fontWeight: "400"}} align="left">Total new corona deaths during the last 3 days"</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.covidData.map((row, index) => (
+                            <TableRow key={'covidRow' + index}>
+
+                                <TableCell style={{fontWeight: "800"}} className="stateData" align="left">{states[row.usState]}</TableCell>
+                                <TableCell className="hospitalizedData"align="left">{row.hospitalized || 'No data'}</TableCell>
+                                <TableCell className="deathData"align="left">{row.death || 'No data'}</TableCell>
+
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>)
     }
 
     render() {
 
         return (
             <div>
-                <h1 className="header-title"><span className="header-title-main">Covid-19:</span> Current hospitilized in US states </h1>
-                <Button>Get more info</Button>
+                <h1 style={{fontWeight: "300"}}><span style={{fontWeight: "500"}} >Covid-19:</span> Live Corona statistics of the U.S</h1>
                 {this.covidDataElements()}
             </div >
         );
@@ -91,27 +79,3 @@ class Covid extends Component {
 }
 
 export default Covid;
-
-
-
-// this.setState({
-//     usState: response.data[0].usState,
-//     hospitalizedCurrently: response.data[0].hospitalized
-// })
-
-
-//  for (let i = 0; i < response.data.length; i++) {
-
-// const hospitalizedCurrently2 = response.data[i].hospitalized;
-// const usState2 = response.data[0].usState;
-// console.log("hospitalized222: ", hospitalizedCurrently2, "state222: ", usState2);
-
-// // axios.get("/death").then(response => {
-// //     const death = response.data[0].totalDeaths;
-// //     const usState = response.data[0].usState;
-// //     console.log("DATA number: ", death, "DATA state: ", usState);
-// //     console.log("PATH (death): ", response);
-
-
-
-// // });
