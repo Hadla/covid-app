6c2aa68221d8a1b48a661dcb52e5413d48f59341
Hadla <hadla.bergman@gmail.com>
2020-05-11 18:30:17 +0200

    Displayed table in fronend.

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const fetch = require('node-fetch')
+const moment = require('moment');
+
 const app = express();
 const port = 5000;
 
 
-
+// Currently hospitalized per US state
 app.get('/current', async function (req, res) {
     try {
         const currentResponse = await fetch('https://covidtracking.com/api/v1/states.json')
         const currentData = await currentResponse.json();
 
-        // Currently hospitalized per US state
         const currentResult = currentData.map((element) => {
             return {
                 usState: element.state,
@@ -33,17 +34,21 @@ app.get('/death', async function (req, res) {
         fetch('https://covidtracking.com/api/v1/states/daily.json')
             .then(res => res.json())
             .then(data => {
-                let dateFiltered = data.filter((el) => el.date > 20200507)
+                
+                const threeDaysAgo = moment().subtract(3, "day").format('YYYYMMDD');
+                console.log('THE DATE 3 DAYS AGO: ', threeDaysAgo);
+                
+                let dateFiltered = data.filter((el) => el.date > threeDaysAgo)
                 let states = [];
                 for (let i = 0; i < dateFiltered.length; i++) {
                     if (!states.includes(dateFiltered[i].state)) {
                         states.push(dateFiltered[i].state);
                     }
                 }
-                let result = states.map(shortName => {
-                    let thisStateData = dateFiltered.filter(dateNumbers => shortName === dateNumbers.state);
+                let result = states.map(element => {
+                    let thisStateData = dateFiltered.filter(dateNumbers => element === dateNumbers.state);
                     return {
-                        state: shortName,
+                        usState: element,
                         totalDeaths: thisStateData.map(items => items.deathIncrease)
                             .reduce((prev, curr) => prev + curr, 0)
                     }
@@ -56,30 +61,5 @@ app.get('/death', async function (req, res) {
     }
 });
 
-
-
-
-// app.get('/death', async function (req, res) {
-//     try {
-
-//         const deathResponse = await fetch('https://covidtracking.com/api/v1/states/daily.json')
-//         const deathData = await deathResponse.json();
-
-//         const deathResult = deathData.map((element) => {
-//             return {
-//                 usState: element.state,
-//                 death: element.deathIncrease
-//             }
-//         });
-
-//         const result = [...deathResult]
-
-//         console.log('result', result)
-//         res.json(result)
-//     } catch (error) {
-//         console.log(error.message)
-//     }
-// })
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`Covid app listening at http://localhost:${port}`));
 
--- frontend/src/Covid.js
+++ frontend/src/Covid.js
@@ -2,47 +2,103 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 class Covid extends Component {
     constructor() {
         super();
         this.state = {
             usState: "Alaska",
-            hospitalized: "five",
-            death: "two"
+            hospitalizedCurrently: "five",
+            totalDeaths: "two",
+            covidData: []
         }
     }
 
     componentDidMount = () => {
-        axios.get("/current").then(response => {
-            const hospitalizedCurrently = response.data[0].hospitalized;
-            const usState = response.data[0].usState;
-            console.log("DATA hospitalized: ", hospitalizedCurrently, "DATA state: ", usState);
-            console.log("PATH (current): ", response);
-            // this.setState({
-            //     usState: response.data.state
-            // })
+        this.getCovidData()
 
-        });
-        axios.get("/death").then(response => {
-            const death = response.data[0].death;
-            const usState = response.data[0].usState;
-            console.log("DATA number: ", death, "DATA state: ", usState);
-            console.log("PATH (death): ", response);
-            
+    }
 
-
-        });
+    getCovidData() {
+        return axios.get("/current")
+            .then(body => {
+                console.log(body);
+                this.setState({
+                    ...this.state,
+                    covidData: body.data
+                })
+            })
+    }
+    covidDataElements() {
+        console.log(this.state.covidData);
+        return (
+        <TableContainer component={Paper}>
+      <Table className="table" aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell >State</TableCell>
+            <TableCell align="left">Hospitalized</TableCell>
+            <TableCell align="left">Death</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {this.state.covidData.map((row) => (
+            <TableRow key={row.name}>
+              
+              <TableCell align="left">{row.usState}</TableCell>
+              <TableCell align="left">{row.hospitalized || 'No data'}</TableCell>
+              <TableCell align="left">{'Placeholder' || 'No data'}</TableCell>
+ 
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>)
+       
     }
 
     render() {
+
         return (
             <div>
                 <h1 className="header-title"><span className="header-title-main">Covid-19:</span> Current hospitilized in US states </h1>
                 <Button>Get more info</Button>
-                <p>{this.state.usState} have___ {this.state.hospitilized} patients currently hospitilized.</p>
-            </div>
+                {this.covidDataElements()}
+            </div >
         );
     }
 }
 
 export default Covid;
+
+
+
+// this.setState({
+//     usState: response.data[0].usState,
+//     hospitalizedCurrently: response.data[0].hospitalized
+// })
+
+
+//  for (let i = 0; i < response.data.length; i++) {
+
+// const hospitalizedCurrently2 = response.data[i].hospitalized;
+// const usState2 = response.data[0].usState;
+// console.log("hospitalized222: ", hospitalizedCurrently2, "state222: ", usState2);
+
+// // axios.get("/death").then(response => {
+// //     const death = response.data[0].totalDeaths;
+// //     const usState = response.data[0].usState;
+// //     console.log("DATA number: ", death, "DATA state: ", usState);
+// //     console.log("PATH (death): ", response);
+
+
+
+// // });
